87a83e04b4aa50949d37103872185cf80aea0903
janaMXCX <chenxingmuxue@outlook.com>
2023-11-15 20:21:00 +0800

    first commit

---
+++ 顺序存储/Sqlist.py
@@ -0,0 +1,14 @@
+
+lis = [1, 2, 3, 4]
+
+lis.append(10)  # 列表的尾部插入
+print(lis)
+lis.insert(1, 20)  # 在索引为1的位置上插入
+print(lis)
+lis.remove(3)  # 删除
+print(lis)
+lis[4] = 30  # 替换
+print(lis)
+
+i = lis.index(20)  # 查找
+print(i)
